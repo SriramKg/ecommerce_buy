999495ab42f19fdb45767f43e74c8642eb43c657
Sriram K G <kgsriram99@gmail.com>
2024-06-07 09:21:56 +0530

    ATC from WishPage feature

--- src/pages/WishPage.jsx
+++ src/pages/WishPage.jsx
@@ -1,41 +1,117 @@
 import { useSelector, useDispatch } from "react-redux";
 import AppLayout from "./AppLayout";
 import { deleteFromWish } from "../store/wishSlice";
-import { Col, Layout, Row, Image, Card, Button, Divider, notification } from "antd";
+import { addToCart, updateToCart } from "../store/cartSlice";
+import {
+  Col,
+  Layout,
+  Row,
+  Image,
+  Card,
+  Button,
+  Divider,
+  notification,
+} from "antd";
 
 const WishPage = () => {
   const wish = useSelector((state) => state.wish.wishList);
+  const cart = useSelector((state) => state.cart.cartItem);
   const [api, contextHolder] = notification.useNotification();
   const dispatch = useDispatch();
   console.log("wishpage", wish);
 
   const deleteWish = (product) => {
-    dispatch(deleteFromWish({
-        product: product.product,
-    }))
-  }
+    dispatch(
+      deleteFromWish({
+        product: product,
+        quantity: 1,
+      })
+    );
+  };
 
   const openNotificationWithIcon = (type) => {
     api[type]({
-      message: 'Product Removed from Wishlist!!',
-      });
+      message: "Product Removed from Wishlist!!",
+    });
+  };
+  const openNotificationWithIconSuccess = (type) => {
+    api[type]({
+      message: "Product Added to Cart!!",
+    });
+  };
+
+  const handleCart = (product) => {
+    const isProductExisting = cart.findIndex(
+      (item) => item.product.id === product.id
+    );
+    console.log(isProductExisting);
+    console.log(cart[isProductExisting]);
+    if (isProductExisting !== -1) {
+      dispatch(
+        updateToCart({
+          product: product,
+          quantity: cart[isProductExisting].quantity + 1,
+        })
+      );
+      dispatch(
+        deleteFromWish({
+          product: product,
+          quantity: 1,
+        })
+      );
+    } else {
+      dispatch(
+        addToCart({
+          product: product,
+          quantity: 1,
+        })
+      );
+      dispatch(
+        deleteFromWish({
+          product: product,
+          quantity: 1,
+        })
+      );
+    }
   };
 
   const wishDetails = wish.map((product) => (
-    <Col xs={24} sm={12} md={8} lg={6} key={product.product.id} style={{ padding: '8px' }}>
+    <Col
+      xs={24}
+      sm={12}
+      md={8}
+      lg={8}
+      xl={12}
+      key={product.product.id}
+      style={{ padding: "8px" }}
+    >
       <Card>
         <Image width="100%" src={product.product.images[0]} height={200} />
         <p>{product.product.title}</p>
         <p>Price : ${product.product.price}</p>
         <p>Product Rating : {product.product.rating}</p>
         <Divider />
-        <Button type="primary">Move to Cart</Button>
+        <Button
+          type="primary"
+          onClick={() => {
+            openNotificationWithIconSuccess("success");
+            handleCart(product.product);
+          }}
+        >
+          Move to Cart
+        </Button>
         <br />
         <br />
         {contextHolder}
-        <Button onClick={() => {openNotificationWithIcon('info'); deleteWish(product)}} style={{color: 'white', backgroundColor: 'hotpink'}}>
+        <Button
+          onClick={() => {
+            openNotificationWithIcon("info");
+            deleteWish(product.product);
+          }}
+          style={{ color: "white", backgroundColor: "hotpink" }}
+        >
           Remove from WishList
-          </Button>
+        </Button>
       </Card>
     </Col>
   ));
@@ -52,4 +128,3 @@ const WishPage = () => {
 };
 
 export default WishPage;
-
